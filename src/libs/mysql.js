eaacd9671c7abf77b3e9a2a5d861601e60423f08
Alanoxer <Johnsito616@gmail.com>
2024-06-27 22:04:58 -0300

    #

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -4,6 +4,6 @@ export const conn = await mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  port: parseInt(process.env.MYSQL_PORT),
+  port: process.env.MYSQL_PORT,
   database: "shopestdb",
 });

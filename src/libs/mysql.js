f78467e6cfbcad6d6442d590cc3d1ef2fd28d509
Alanoxer <Johnsito616@gmail.com>
2024-07-01 20:29:24 -0300

    #

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -1,9 +1,9 @@
 import mysql from "mysql2/promise";
 
 export const conn = await mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  port: process.env.MYSQL_PORT,
+  host: "shopestdb.crwsssa8k56d.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "polaco12",
+  port: parseInt(3306),
   database: "shopestdb",
 });

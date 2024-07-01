01cb1d00e55359890ae11d96a2e573023cbe3993
Alanoxer <Johnsito616@gmail.com>
2024-07-01 20:24:28 -0300

    #

--- src/app/products/page/[pagination]/page.jsx
+++ src/app/products/page/[pagination]/page.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 async function LoadProducts(pagination){
   
-  const products = await axios.get(`https://shopest-lyart.vercel.app/api/products`,
+  const products = await axios.get(`/api/products`,
   {
     params:{
       pagination : pagination

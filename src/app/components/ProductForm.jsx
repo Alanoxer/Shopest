0f2c997ab3dc5f7f620e93128e96ed0d8da48d3a
Alanoxer <Johnsito616@gmail.com>
2024-07-03 16:02:34 -0300

    #

--- src/app/components/ProductForm.jsx
+++ src/app/components/ProductForm.jsx
@@ -46,7 +46,7 @@ export default function ProductForm() {
     }
 
     if (!params.id) {
-      const res = await axios.post(`/api/products`, formData, {
+      const res = await axios.post(`https://shopest-lyart.vercel.app/api/products`, formData, {
         headers: {
           'Content-Type': `multipart/form-data`,
         },

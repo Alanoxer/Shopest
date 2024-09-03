78d1f293ced92d27ed30be62b8e6a9cc7604843c
Alanoxer <Johnsito616@gmail.com>
2024-09-03 00:05:44 -0300

    #

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -2,12 +2,15 @@
 // import  {conn}  from "../../libs/mysql"
 // import { CalendarDays, MapPin, Link as LinkIcon } from "lucide-react"
 
-import Profile from "../components/Profile"
+import GetSession from "../components/GetSession"
 
-export default function ProfilePage(){
+export default async function ProfilePage(){
+  const user = await GetSession()
+  console.log(user)
+
   return (
     <>
-    <Profile/>
+    hola
     </>
   )
 }

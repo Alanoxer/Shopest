d68fc3e73c7492b555ce9256cdf5e76376578908
Alanoxer <Johnsito616@gmail.com>
2024-09-22 15:53:14 -0300

    #

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -72,7 +72,7 @@ export default function LoginPage(){
             </div>
             <div className="mt-6 grid grid-cols-2 gap-4">
               <Button className="bg-blue-600 hover:text-blue-600 " variant="outline" type="button">
-                <Link href={"http://localhost:3000/api/auth/callback/google"}>
+                <Link href={"/api/auth/callback/google"}>
                 <Facebook className="mr-2 h-4 w-4 text-white " />
                 Facebook
                 </Link>

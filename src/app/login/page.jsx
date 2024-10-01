5d91609ec50524d7a279a067791219d9f06b4435
Alanoxer <Johnsito616@gmail.com>
2024-10-01 17:58:03 -0300

    #

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -76,13 +76,17 @@ export default function LoginPage(){
               </div>
             </div>
             <div className="mt-6 grid grid-cols-2 gap-4">
-              <Button className="bg-blue-600 hover:text-blue-600 " variant="outline" type="button">
-                <Link href={"/api/auth/callback/google"}>
+              <Button onClick={async()=>{
+                signIn("google")
+              }} className="bg-blue-600 hover:text-blue-600 " variant="outline" type="button"> 
                   <Facebook className="mr-2 h-4 w-4 text-white " />
                     Facebook
-                </Link>
               </Button>
-              <Button variant="outline" type="button">
+              <Button onClick={async()=>{
+                const result = await signIn("google")
+                console.log(result)
+              }}
+              variant="outline" type="button">
                 <svg className="mr-2 h-4 w-4" viewBox="0 0 24 24">
                   <path
                     d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
